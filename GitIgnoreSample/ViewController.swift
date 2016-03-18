a774a9d9ab90050dd981a667594034d526140863
Akshay <akshay.kumar@appster.in>
2016-03-18 18:47:47 +0530

    my first commit

--- GitIgnoreSample/ViewController.swift
+++ GitIgnoreSample/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let a = "ad"
+        print(a)
     }
 
     override func didReceiveMemoryWarning() {
